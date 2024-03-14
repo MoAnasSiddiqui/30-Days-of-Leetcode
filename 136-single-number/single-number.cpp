2e7fc83b20a070f3cc03efec9ada2476e5ab03e2
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-14 14:23:06 +0500

    Time: 26 ms (15.31%) | Memory: 23.6 MB (5.36%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> umap;
-        for (auto i = 0; i < nums.size(); i++) {
-            if (umap.count(nums[i]) == 0) {
-                umap.insert({nums[i], 1});
-            } else {
+        unordered_map<int,int> umap;
+        for (auto i = 0; i<nums.size(); i++){
+            if (umap.count(nums[i]) == 0){
+                umap.insert({nums[i],1});
+            }
+            else{
                 umap[nums[i]] = 2;
             }
         }
-        for (auto i = 0; i < nums.size(); i++) {
-            if (umap[nums[i]] == 1) {
-                return nums[i];
-            }
+        for (auto i = 0; i<nums.size(); i++){
+            if (umap[nums[i]] == 1){return nums[i];}
         }
         return 0;
     }
