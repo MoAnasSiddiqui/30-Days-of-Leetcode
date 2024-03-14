c8d6ef590c5b1db9af56a75462f74fc1f9a9d2ea
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-14 15:06:14 +0500

    Time: 8 ms (59.22%) | Memory: 8.2 MB (72.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0){return false;}
-        else {
-            vector<int> temp;
-            while (x != 0){
-                temp.push_back(x%10);
-                x=x/10;
-            }
-            for (auto i=0; i<temp.size()/2; i++){
-                if (temp[i] != temp[temp.size()-1-i]){
-                    return false;
-                }
-            }
+        unsigned int reversed = 0;
+        int temp = x;
+        while (x != 0){
+            reversed = (reversed*10) + (x%10);
+            x = x/10;
         }
-        return true;
+        if (reversed == temp){return true;}
+        else{return false;}
     }
 };
