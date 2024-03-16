920569c1a48796ae63f69338c49ac59d483cf349
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-16 14:21:56 +0500

    Time: 0 ms (100.00%) | Memory: 7.3 MB (13.87%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        unsigned int square;
-        if (num == 1 || num == 4){return true;}
-        else if (num < 4){return false;}
-        else{
-            for (int i = 2; i < num/2; i++){
-                if (i == 46341){break;}
-                square = i*i;
-                if (square == num){return true;}
-                else if (square > num){break;}
-            }
-        }
-        return false;
+        if (sqrt(num) == floor(sqrt(num))){return true;}
+        else {return false;}
+        // unsigned int square;
+        // if (num == 1 || num == 4){return true;}
+        // else if (num < 4){return false;}
+        // else{
+        //     for (int i = 2; i < num/2; i++){
+        //         if (i == 46341){break;}
+        //         square = i*i;
+        //         if (square == num){return true;}
+        //         else if (square > num){break;}
+        //     }
+        // }
+        // return false;
     }
 };
