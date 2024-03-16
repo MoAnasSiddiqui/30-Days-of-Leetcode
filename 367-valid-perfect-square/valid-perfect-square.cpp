c007823095f8483416506c671ca9871b07494d33
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-16 15:09:58 +0500

    Time: 2 ms (56.28%) | Memory: 7 MB (52.25%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        if (sqrt(num) == floor(sqrt(num))){return true;}
-        else {return false;}
-        // unsigned int square;
-        // if (num == 1 || num == 4){return true;}
-        // else if (num < 4){return false;}
-        // else{
-        //     for (int i = 2; i < num/2; i++){
-        //         if (i == 46341){break;}
-        //         square = i*i;
-        //         if (square == num){return true;}
-        //         else if (square > num){break;}
-        //     }
-        // }
-        // return false;
+        int square;
+        if (num == 1){return true;}
+        else{
+            for (int i = 2; i <= num/2; i++){
+                if (i == 46341){break;}
+                square = i*i;
+                if (square == num){return true;}
+                else if (square > num){break;}
+            }
+        }
+        return false;
     }
 };
+// class Solution {
+// public:
+//     bool isPerfectSquare(int num) {
+//         if (num == 1){return true;}
+//         int left = 0;
+//         int right = num;
+//         int mid;
+//         while (left < right){
+//             mid = (left+right)/2;
+//             int square;
+//             if (mid < 46341){square = mid*mid;}
+//             if (square == num){return true;} 
+//             else if (square > num){right = mid - 1;}
+//             else {left = mid + 1;}
+//         }
+//         return false;
+//     }
+// };
