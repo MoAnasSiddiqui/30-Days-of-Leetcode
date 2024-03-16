89aef1a31eb37073827200e668b6216c571c2ac3
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-16 06:19:30 +0500

    Time: 0 ms (100.00%) | Memory: 7.1 MB (63.92%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        int counter = 31;
+        while (n > 0){
+            ans = ans + (pow(2, counter) * (n & 1));
+            n = (n >> 1);
+            counter--;
+        }
+        return ans;
+    }
+};
+
+// 00000010100101000001111010011100
+// 00111001011110000010100101000000
