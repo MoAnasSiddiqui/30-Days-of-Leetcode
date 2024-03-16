db9fc78bca271dac7ee2ea0475a8b524a8a19ac9
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-16 15:26:48 +0500

    Time: 3 ms (57.71%) | Memory: 12.7 MB (19.21%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,11 +1,28 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int ans = INT_MAX;
+//         for (auto i = 0; i<nums.size(); i++){
+//             if (nums[i] < ans){ans = nums[i];}
+//             if (nums[(i+nums.size()-1)%(nums.size())] > nums[i]){break;}
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int ans = INT_MAX;
-        for (auto i = 0; i<nums.size(); i++){
-            if (nums[i] < ans){ans = nums[i];}
-            if (nums[(i+nums.size()-1)%(nums.size())] > nums[i]){break;}
+        int l = 0, r = nums.size() - 1;
+
+        while (nums[l] > nums[r]) {
+            int m = (l + r) / 2;
+
+            if (nums[l] <= nums[m])
+                l = m + 1;
+            else
+                r = m;
         }
-        return ans;
+
+        return nums[l];
     }
 };
