0547180c6ce010c43a852d26d883931c08102933
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-16 15:15:53 +0500

    Time: 3 ms (24.99%) | Memory: 7 MB (84.37%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,34 +1,32 @@
-class Solution {
-public:
-    bool isPerfectSquare(int num) {
-        int square;
-        if (num == 1){return true;}
-        else{
-            for (int i = 2; i <= num/2; i++){
-                if (i == 46341){break;}
-                square = i*i;
-                if (square == num){return true;}
-                else if (square > num){break;}
-            }
-        }
-        return false;
-    }
-};
 // class Solution {
 // public:
 //     bool isPerfectSquare(int num) {
+//         int square;
 //         if (num == 1){return true;}
-//         int left = 0;
-//         int right = num;
-//         int mid;
-//         while (left < right){
-//             mid = (left+right)/2;
-//             int square;
-//             if (mid < 46341){square = mid*mid;}
-//             if (square == num){return true;} 
-//             else if (square > num){right = mid - 1;}
-//             else {left = mid + 1;}
+//         else{
+//             for (int i = 2; i <= num/2; i++){
+//                 if (i == 46341){break;}
+//                 square = i*i;
+//                 if (square == num){return true;}
+//                 else if (square > num){break;}
+//             }
 //         }
 //         return false;
 //     }
 // };
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 1){return true;}
+        int left = 1;
+        int right = num/2;
+        long long mid;
+        while (left <= right){
+            mid = (left+right)/2;
+            if (mid*mid == num){return true;} 
+            else if (mid*mid > num){right = mid - 1;}
+            else {left = mid + 1;}
+        }
+        return false;
+    }
+};
