e34d09f5a03a80d08d5173afe16cac89819dc093
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-18 20:12:56 +0500

    Time: 3 ms (80.26%) | Memory: 11.6 MB (65.24%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,18 +1,18 @@
-// class Solution {
-// public:
-//     ListNode* reverseList(ListNode* head) {
-//         if (head == nullptr){return head;}
-//         return solution(head, nullptr);
-//     }
-//     ListNode* solution (ListNode* head, ListNode* prev){
-//         if (head == nullptr){
-//             return prev;
-//         }
-//         ListNode* nxt = head->next;
-//         head->next = prev;
-//         return solution(nxt, head);
-//     }
-// };
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr){return head;}
+        return solution(head, nullptr);
+    }
+    ListNode* solution (ListNode* head, ListNode* prev){
+        if (head == nullptr){
+            return prev;
+        }
+        ListNode* nxt = head->next;
+        head->next = prev;
+        return solution(nxt, head);
+    }
+};
 
 // class Solution {
 // public:
@@ -28,21 +28,3 @@
 //         return prev;
 //     }
 // };
-
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-
-        if(head == NULL){
-            return NULL;
-        }
-
-        ListNode* newHead = head;
-        if(head->next != NULL){
-            newHead = reverseList(head ->next);
-            head->next->next = head;
-        }
-        head->next = NULL;
-        return newHead;
-    }    
-};
