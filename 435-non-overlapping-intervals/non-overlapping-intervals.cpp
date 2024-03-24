db705f4f4263cd06ab7d3d00595cd758fa14af70
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-24 21:09:19 +0500

    Time: 228 ms (87.78%) | Memory: 93.3 MB (47.80%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,12 +1,26 @@
+// class Solution {
+// public:
+//     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(), intervals.end());
+//         int last = (intervals.size())-1;
+//         int ans = 0;
+//         int size = intervals.size()-2;
+//         for (auto i = size; i>=0; i--){
+//             if (intervals[i][1] > intervals[last][0]){ans ++;}
+//             else{last = i;}
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
+    static bool solve(vector<int>&a , vector<int> &b){return a[1] < b[1];}
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        int last = (intervals.size())-1;
+        sort(intervals.begin(), intervals.end(),solve);
         int ans = 0;
-        int size = intervals.size()-2;
-        for (auto i = size; i>=0; i--){
-            if (intervals[i][1] > intervals[last][0]){ans ++;}
+        int last = 0;
+        for (int i = 1; i<intervals.size(); i++){
+            if (intervals[i][0] < intervals[last][1]){ans ++;}
             else{last = i;}
         }
         return ans;
