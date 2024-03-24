ab1d29102e3429a8a602123ddbb152bcd59618ad
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-24 05:55:54 +0500

    Time: 21 ms (74.01%) | Memory: 22.7 MB (41.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,50 +1,19 @@
-// class Solution {
-// public:
-//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-//         sort(intervals.begin(), intervals.end());
-//         for (auto i=1; i<intervals.size(); i++){
-//             if (intervals[i][0] <= intervals[i-1][1]){
-//                 if(intervals[i][1] > intervals[i-1][1]){intervals[i-1][1] = intervals[i][1];}
-//                 intervals.erase(intervals.begin()+i);
-//                 i--;
-//             }
-//         }
-//         return intervals;
-//     }
-// };
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-      if(intervals.size()==1)
-         return intervals;
-      vector<pair<int,int>> p;
-      for(int i=0;i<intervals.size();i++)
-      {
-          p.push_back({intervals[i][0],intervals[i][1]});
-      } 
-      sort(p.begin(),p.end());
-
-      vector<vector<int>> ans;
-      int f=p[0].first,s=p[0].second;
-      for(int i=0;i<p.size()-1;i++)
-      {
-          vector<int> a(2);
-          if(s>=p[i+1].first)
-          {
-              s=max(s,p[i+1].second);
-          }
-          else
-          {
-              a[0]=f;
-              a[1]=s;
-              f=p[i+1].first;
-              s=p[i+1].second;
-              ans.push_back(a);
-          }
-      } 
-      int n=intervals.size();
-      ans.push_back({f,s});
-      return ans;
+        sort(intervals.begin(), intervals.end());
+        int last = 0;
+        for (auto i=1; i<intervals.size(); i++){
+            if (intervals[i][0] <= intervals[last][1]){
+                if(intervals[i][1] > intervals[last][1]){intervals[last][1] = intervals[i][1];}
+                intervals[i] = {-1,-1};
+            }
+            else {last = i;}
+        }
+        vector<vector<int>> ans;
+        for(int i = 0; i < intervals.size(); i++){
+            if (intervals[i][0] != -1){ans.push_back(intervals[i]);}
+        }
+        return ans;
     }
 };
