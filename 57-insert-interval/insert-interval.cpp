8f5a6e36ed763f0ba80b874fc32da6e8bb56b3ad
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-24 15:57:50 +0500

    Time: 16 ms (21.44%) | Memory: 21 MB (10.67%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,68 +1,68 @@
-// #pragma GCC optimize("O3", "unroll-loops")
-class Solution {
-public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
-    {
-        vector<vector<int>> ans;
-        int a=newInterval[0], b=newInterval[1];
-        int i=0, n=intervals.size();
-        
-        // Insert intervals before the new interval or non-overlapping intervals
-        for ( ; i < n && intervals[i][1]<a; i++) 
-            ans.push_back(intervals[i]);
-        
-        
-        // Merge overlapping intervals
-        for ( ; i < n && intervals[i][0] <= b; i++) {
-            a = min(a, intervals[i][0]);
-            b = max(b, intervals[i][1]);    
-        }
-        
-        ans.push_back({a, b}); // Insert the merged interval
-        
-        // Insert the remaining intervals
-        ans.reserve(ans.size()+intervals.size()-i);
-        ans.insert(ans.end(), intervals.begin()+i, intervals.end());
-
-        return ans;
-    }
-};
-
+#pragma GCC optimize("O3", "unroll-loops")
 // class Solution {
 // public:
-//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+//     {
 //         vector<vector<int>> ans;
-//         bool insert = false;
-//         for (auto i : intervals){
-//             if (newInterval[1] < i[0] && insert == false){
-//                 ans.push_back(newInterval);
-//                 ans.push_back(i);
-//                 insert = true;
-//             } 
-//             else if(newInterval[1] >= i[0] && newInterval[1] <= i[1] && insert == false){
-//                 if (newInterval[0] < i[0]){i[0] = newInterval[0];}
-//                 insert = true;
-//                 ans.push_back(i);
-//             } 
-//             else if(newInterval[0] >= i[0] && newInterval[0] <= i[1] && insert == false){
-//                 if (newInterval[1] > i[1]){i[1] = newInterval[1];}
-//                 insert = true;
-//                 ans.push_back(i);
-//             }
-//             else if (i[0] >= newInterval[0] && i[1] <= newInterval[1] && insert == false){
-//                 ans.push_back(newInterval);
-//                 insert = true;
-//             }
-//             else if(insert == false){ans.push_back(i);}
-//             else if (insert == true){
-//                 if (i[0] <= ans.back()[1] && i[1] > ans.back()[1]){ans.back()[1] = i[1];}
-//                 else if (i[0] > ans.back()[1]){ans.push_back(i);}
-//             }
+//         int a=newInterval[0], b=newInterval[1];
+//         int i=0, n=intervals.size();
+        
+//         // Insert intervals before the new interval or non-overlapping intervals
+//         for ( ; i < n && intervals[i][1]<a; i++) 
+//             ans.push_back(intervals[i]);
+        
+        
+//         // Merge overlapping intervals
+//         for ( ; i < n && intervals[i][0] <= b; i++) {
+//             a = min(a, intervals[i][0]);
+//             b = max(b, intervals[i][1]);    
 //         }
-//         if (insert == false){ans.push_back(newInterval);}
+        
+//         ans.push_back({a, b}); // Insert the merged interval
+        
+//         // Insert the remaining intervals
+//         ans.reserve(ans.size()+intervals.size()-i);
+//         ans.insert(ans.end(), intervals.begin()+i, intervals.end());
+
 //         return ans;
 //     }
 // };
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        bool insert = false;
+        for (auto i : intervals){
+            if (newInterval[1] < i[0] && insert == false){
+                ans.push_back(newInterval);
+                ans.push_back(i);
+                insert = true;
+            } 
+            else if(newInterval[1] >= i[0] && newInterval[1] <= i[1] && insert == false){
+                if (newInterval[0] < i[0]){i[0] = newInterval[0];}
+                insert = true;
+                ans.push_back(i);
+            } 
+            else if(newInterval[0] >= i[0] && newInterval[0] <= i[1] && insert == false){
+                if (newInterval[1] > i[1]){i[1] = newInterval[1];}
+                insert = true;
+                ans.push_back(i);
+            }
+            else if (i[0] >= newInterval[0] && i[1] <= newInterval[1] && insert == false){
+                ans.push_back(newInterval);
+                insert = true;
+            }
+            else if(insert == false){ans.push_back(i);}
+            else if (insert == true){
+                if (i[0] <= ans.back()[1] && i[1] > ans.back()[1]){ans.back()[1] = i[1];}
+                else if (i[0] > ans.back()[1]){ans.push_back(i);}
+            }
+        }
+        if (insert == false){ans.push_back(newInterval);}
+        return ans;
+    }
+};
 // class Solution {
 // public:
 //     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
