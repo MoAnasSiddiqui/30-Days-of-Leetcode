5878a784afe3a6cbf91be1c8c0ffd679b07dc007
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-31 03:51:31 +0500

    Time: 4 ms (93.43%) | Memory: 16.1 MB (77.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int,int> rows;
-        unordered_map<int,int> cols; 
+        bool flag_row = false;
+        bool flag_col = false;
         for (auto i = 0; i<matrix.size(); i++){
+            if (matrix[i][0] == 0){flag_col = true;}
             for (auto j = 0; j<matrix[0].size(); j++){
+                if (i == 0 && matrix[i][j] == 0){flag_row = true;}
                 if (matrix[i][j] == 0){
-                    rows[i] = 1;
-                    cols[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (auto i = 0; i<matrix.size(); i++){
-            for (auto j = 0; j<matrix[0].size(); j++){
-                if (rows[i] == 1 || cols[j] == 1){matrix[i][j] = 0;}
+        for (auto i = 1; i<matrix.size(); i++){
+            if (matrix[i][0] == 0){
+                for (auto j = 1; j<matrix[0].size(); j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (auto j = 1; j<matrix[0].size(); j++){
+            if (matrix[0][j] == 0){
+                for (auto i = 1; i<matrix.size(); i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (flag_row == true){
+            for (int i = 0; i<matrix[0].size(); i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if (flag_col == true){
+            for (int i = 0; i<matrix.size(); i++){
+                matrix[i][0] = 0;
             }
         }
     }
