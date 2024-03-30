cb3bd7c46b37b59efdefd0529c75399c648c6df8
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-03-30 16:16:50 +0500

    Time: 16 ms (53.88%) | Memory: 11.1 MB (72.50%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,19 +3,19 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char,int> umap;
         int ans = 0;
-        int count = 0;
-        for (int i = 0; i<s.size(); i++){
-            if (umap[s[i]] == 0){
-                count ++ ;
+        int start = 0;
+        for (auto i = 0; i<s.size(); i++){
+            if (umap[s[i]] <= start){
+                umap[s[i]] = i+1;
+            } else {
+                ans = max(ans, i-start);
+                start = umap[s[i]];
                 umap[s[i]] = i+1;
             }
-            else {
-                ans = max(ans,count);
-                i = umap[s[i]] - 1;
-                umap.clear();
-                count = 0;
-            }
         }
-        return max(count,ans);
+        if (s.size()-start > ans){
+            ans = s.size()-start;
+        }
+        return ans;
     }
 }; 
