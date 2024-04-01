6a7f53f3ca69df19a8807270ab2d2e30045601eb
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-01 05:26:35 +0500

    Time: 23 ms (70.53%) | Memory: 22.1 MB (24.32%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -24,16 +24,5 @@ public:
                 temp = temp->next;
             }
         }
-        
-        // if (head->next == nullptr || head->next->next == nullptr){return;}
-        // ListNode* current = head;
-        // while(current != nullptr){
-        //     ListNode* temp = current;
-        //     while (temp->next != nullptr && temp->next->next != nullptr){
-        //         temp = temp->next;
-        //     }
-        //     cout << current->val << " " << temp->val << endl;
-        //     current = current->next;
-        // }
     }
 };
