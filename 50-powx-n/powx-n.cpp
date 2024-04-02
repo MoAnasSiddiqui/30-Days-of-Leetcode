d06d8a9f3855b06bac5b651a81e23f68878bc030
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-02 16:01:51 +0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.69%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (x == 0 || x == 1 || (x==-1 && n%2==1)){return x;}
-        if (n == 0){return 1;}
-        if (x == -1 && n%2 == 0){return 1;}
+        // if (x == 0 || x == 1 || (x==-1 && n%2==1)){return x;}
+        // if (n == 0){return 1;}
+        // if (x == -1 && n%2 == 0){return 1;}
 
-        if (n < 0){x = 1/x;}
-        if (n == INT_MIN){n = INT_MAX;}
-        else {n = abs(n);}
+        // if (n < 0){x = 1/x;}
+        // if (n == INT_MIN){n = INT_MAX;}
+        // else {n = abs(n);}
         
-        double ans = x;
-        for (auto i = 1; i < n; i++){
-            if (i < n/2){
-                ans = ans*ans;
-                i = (2*i)-1;
-            } else {ans = ans*x;}
+        // double ans = x;
+        // for (auto i = 1; i < n; i++){
+        //     if (i < n/2){
+        //         ans = ans*ans;
+        //         i = (2*i)-1;
+        //     } else {ans = ans*x;}
             
-            if (ans == 0){break;}
-        }
+        //     if (ans == 0){break;}
+        // }
 
-        return ans;
+        return pow(x,n);
     }
 };
