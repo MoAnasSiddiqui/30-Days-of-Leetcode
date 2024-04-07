45eaa930fcf4cc403a4ddeb6c38a59ad86b0df0d
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-07 23:16:50 +0500

    Time: 56 ms (5.65%) | Memory: 17.1 MB (78.81%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans(k+1);
+        unordered_map<int,int> umap;
+        for (auto i: nums){umap[i]++;}
+        for (auto i: umap){cout << i.first << " " << i.second << endl;}
+        for (auto i: umap){
+            ans[k] = i.first;
+            for (int j = k-1; j>=0; j--){
+                if (umap[ans[j+1]]>=umap[ans[j]]){
+                    int temp = ans[j+1];
+                    ans[j+1] = ans[j];
+                    ans[j] = temp;
+                } else {break;}
+            }
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
