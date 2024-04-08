646d495c0e3c30596c47a3053411e727dad73d47
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-08 20:02:49 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (55.97%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,42 +1,27 @@
-// class Solution {
-// public:
-//     int divide(int dividend, int divisor) {
-//         if(dividend==INT_MIN && divisor==-1) return INT_MAX;
-//         if(dividend==INT_MIN && divisor==1) return INT_MIN;
-
-//         bool sign = (dividend<0 && divisor>0) || (dividend>0 && divisor<0);
-//         long dd = abs(dividend), dv = abs(divisor);
-//         int count = 1;
-
-//         while (dv <= dd){
-//             while (dv+dv < dd){
-//                 dv = dv + dv;
-//                 count = count*2;
-//             }
-//         }
-
-//         if (sign == true){return -count;}
-//         else{return count;}
-//     }
-// };
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        if (dividend == divisor) return 1;
         if(dividend==INT_MIN && divisor==-1) return INT_MAX;
         if(dividend==INT_MIN && divisor==1) return INT_MIN;
         if(dividend==INT_MAX && divisor==1) return INT_MAX;
         if(dividend==INT_MAX && divisor==-1) return INT_MIN+1;
 
-        bool sign = (dividend<0 && divisor>0) || (dividend>0 && divisor<0);
-        long long dd = abs(dividend), dv = abs(divisor);
-        long long count = 0;
+        int sign = (dividend<0 && divisor>0) || (dividend>0 && divisor<0)? -1:1;
+        dividend = abs(dividend), divisor = abs(divisor);
+        int count = 0;
         
-        while (dv <= dd){
-            dv = dv + abs(divisor);
-            count++;
+        while (divisor <= dividend){
+            long temp1 = divisor;
+            long temp2 = 1;
+            while (temp1<<1 <= dividend){
+                temp1 = temp1 << 1;
+                temp2 = temp2 << 1;
+            }
+            count = count + temp2;
+            dividend = dividend - temp1;
         }
 
-        if (sign == true){return -count;}
-        else{return count;}
+        return sign*count;
     }
 };
