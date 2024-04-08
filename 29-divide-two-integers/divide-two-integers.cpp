7e57fe707504fd4f259f581e12d59d5ae8fe8b56
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-09 02:24:47 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (56.06%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -4,7 +4,6 @@ public:
         if (dividend == divisor) return 1;
         if(dividend==INT_MIN && divisor==-1) return INT_MAX;
         if(dividend==INT_MIN && divisor==1) return INT_MIN;
-        // if(dividend==INT_MAX && divisor==1) return INT_MAX;
 
         int sign = (dividend<0 && divisor>0) || (dividend>0 && divisor<0)? -1:1;
         dividend = abs(dividend), divisor = abs(divisor);
