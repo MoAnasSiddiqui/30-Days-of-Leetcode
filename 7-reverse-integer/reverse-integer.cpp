fb644ac03f18ddf4a97866b929b31e8b823b391e
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-09 14:27:46 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (15.68%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -3,11 +3,10 @@ public:
     int reverse(int x) {
         int sign = (x < 0) ? -1:1;
         x = abs(x);
-        long ans = 0;
+        int ans = 0;
         while (x != 0){
-            if (ans * 10 >= INT_MAX){return 0;}
-            ans = ans * 10;
-            ans = ans + (x%10);
+            if (ans > INT_MAX/10){return 0;}
+            ans = ans * 10 + (x%10);
             x = x/10;
         }
 
