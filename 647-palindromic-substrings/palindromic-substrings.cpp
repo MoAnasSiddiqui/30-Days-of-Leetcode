0a23a2ce83d24053ed317f6901dffa0eade0525b
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-10 09:44:10 +0500

    Time: 1369 ms (5.02%) | Memory: 487.9 MB (5.19%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -4,14 +4,10 @@ public:
         string rev = s;
         reverse(rev.begin(), rev.end());
         int ans = 0;
-        int top = 0;
 
         for (auto i=0; i<s.size(); i++){
             for (auto j = i+1; j<=s.size(); j++){
-                string temp = rev.substr(rev.size()-j, j-i);
-                if (s.substr(i, j-i) == temp){
-                    ans++;
-                }
+                if (s.substr(i, j-i) == rev.substr(rev.size()-j, j-i)){ans++;}
             }
         }
         return ans;
