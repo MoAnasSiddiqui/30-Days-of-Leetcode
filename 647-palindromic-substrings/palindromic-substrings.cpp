40b28a2239493d4b267fa381faeb6ed5997920b1
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-10 09:41:01 +0500

    Time: 1283 ms (5.02%) | Memory: 487.9 MB (5.19%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
         int ans = 0;
         int top = 0;
 
         for (auto i=0; i<s.size(); i++){
             for (auto j = i+1; j<=s.size(); j++){
-                string rev = s.substr(i, j-i);
-                reverse(rev.begin(), rev.end());
-                if (s.substr(i, j-i) == rev){
+                string temp = rev.substr(rev.size()-j, j-i);
+                if (s.substr(i, j-i) == temp){
                     ans++;
                 }
             }
