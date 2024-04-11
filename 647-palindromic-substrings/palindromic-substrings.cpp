3fa9629b4d802086bfedbb69ce5326bd4b2c16e0
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-12 00:46:34 +0500

    Time: 0 ms (100.00%) | Memory: 7.4 MB (95.44%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -11,11 +11,42 @@ public:
         return count;
     }
     int countSubstrings(string s) {
+        int n = s.length();
         int count = 0;
 
-        for (auto i=0; i<s.size(); i++){
-            count = count + check(s, i, i+1) + check(s, i, i);
+        for(int center=0; center<n; center++) {
+            int left = center;
+            int right = left + 0;
+
+            while( left>=0 && right < n && s[right]==s[left]) {
+                count++;
+                left--;
+                right++;
+            }
+
+            left = center;
+            right = left + 1;
+
+            while( left>=0 && right < n && s[right]==s[left]) {
+                count++;
+                left--;
+                right++;
+            }
         }
         return count;
+        // int ans = 0;
+
+        // Solution: 1
+        // for (auto i=0; i<s.size(); i++){
+        //     count = count + check(s, i, i+1) + check(s, i, i);
+        // }
+
+        // Solution: 2
+        // vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        // for (int i = 0; i < s.size(); ++i){dp[i][i] = true, ans++;}
+
+        // for (int i = 0; i < s.size()-1; i++){if (s[i] == s[i+1]){dp[i][i+1] = true; ans++;}}
+
+        // return ans;
     }
 };
