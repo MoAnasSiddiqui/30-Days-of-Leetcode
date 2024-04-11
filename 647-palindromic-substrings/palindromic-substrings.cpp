49344f16aad5d71f0b3d55f094e2b8213537deed
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-12 01:11:05 +0500

    Time: 31 ms (29.08%) | Memory: 10.2 MB (50.91%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -2,35 +2,43 @@ class Solution {
 public:
     int countSubstrings(string s) {
         // Solution: 1
-        int count = 0;
+        // int count = 0;
 
-        for(int i=0; i<s.size(); i++) {
-            int start = i;
-            int end = i + 0;
+        // for(int i=0; i<s.size(); i++) {
+        //     int start = i;
+        //     int end = i + 0;
 
-            while( start>=0 && end < s.size() && s[end]==s[start]) {
-                count++;
-                start--;
-                end++;
-            }
+        //     while( start>=0 && end < s.size() && s[end]==s[start]) {
+        //         count++;
+        //         start--;
+        //         end++;
+        //     }
 
-            start = i;
-            end = start + 1;
+        //     start = i;
+        //     end = start + 1;
 
-            while( start>=0 && end < s.size() && s[end]==s[start]) {
-                count++;
-                start--;
-                end++;
-            }
-        }
-        return count;
+        //     while( start>=0 && end < s.size() && s[end]==s[start]) {
+        //         count++;
+        //         start--;
+        //         end++;
+        //     }
+        // }
+        // return count;
 
         // Solution: 2
-        // vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
-        // for (int i = 0; i < s.size(); ++i){dp[i][i] = true, ans++;}
+        int ans = 0;
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        for (int i = 0; i < s.size(); ++i){dp[i][i] = true, ans++;}
 
-        // for (int i = 0; i < s.size()-1; i++){if (s[i] == s[i+1]){dp[i][i+1] = true; ans++;}}
+        for (int i = 0; i < s.size()-1; i++){if (s[i] == s[i+1]){dp[i][i+1] = true; ans++;}}
 
-        // return ans;
+        for (int i = 2; i < s.size(); i++){
+            for (int j = 0; j <= s.size()-1-i; j++){
+                // cout << j << " " << i+j << endl;
+                if (s[j] == s[i+j] && dp[j+1][i+j-1]){dp[j][i+j] = true; ans++;}
+            }
+        }
+
+        return ans;
     }
 };
