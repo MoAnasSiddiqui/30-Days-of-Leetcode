564cd88e95858c1548c6a8e184f7cf485d630f54
Mohammad Anas Siddiqui <124179257+MoAnasSiddiqui@users.noreply.github.com>
2024-04-11 10:33:12 +0500

    Time: 1013 ms (5.01%) | Memory: 463.2 MB (5.40%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
+    bool check(string s){
+        int start = 0;
+        int end = s.size()-1;
+        while (start < end){
+            if (s[start] != s[end]){return false;}
+            start ++;
+            end --;
+        }
+        return true;
+    }
     int countSubstrings(string s) {
-        string rev = s;
-        reverse(rev.begin(), rev.end());
         int ans = 0;
 
         for (auto i=0; i<s.size()-1; i++){
             for (auto j = i+2; j<=s.size(); j++){
-                if (s.substr(i, j-i) == rev.substr(rev.size()-j, j-i)){ans++;}
+                ans = ans + check(s.substr(i, j-i));
             }
         }
-        return ans+s.size();;
+        return ans+s.size();
     }
 };
